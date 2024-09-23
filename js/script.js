2384ec2fd2b6ac853787590daaf72feb2a3f0965
ale <alemoura.zyc@gmail.com>
2024-09-23 20:52:41 -0300

    feat: create monthConvert function

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function addDate() {
     const title = document.querySelector('.title');
     const date = new Date();
     let weekDay = date.getDay();
+    let month = date.getMonth();
 
     function weekDayConvert() {
         if (weekDay === 0) {
@@ -23,7 +24,38 @@ function addDate() {
         }
     }
 
+    function monthConvert() {
+        if (month === 0) {
+            month = 'Janeiro'
+        } else if (month === 1) {
+            month = 'Fevereiro'
+        } else if (month === 2) {
+            month = 'Março'
+        } else if (month === 3) {
+            month = 'Abril'
+        } else if (month === 4) {
+            month = 'Maio'
+        } else if (month === 5) {
+            month = 'Junho'
+        } else if (month === 6) {
+            month = 'Julho'
+        } else if (month === 7) {
+            month = 'Agosto'
+        } else if (month === 8) {
+            month = 'Setembro'
+        } else if (month === 9) {
+            month = 'Outubro'
+        } else if (month === 10) {
+            month = 'Novembro'
+        } else if (month === 11) {
+            month = 'Dezembro'
+        } else {
+            month = undefined;
+        }
+    }
+
     weekDayConvert();
+    monthConvert();
 
     title.innerHTML = ``;
 }
